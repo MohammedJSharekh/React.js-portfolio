45d819836abdebad51d6c91bd97588fc5b32003d
MohammedJSharekh <59140046+MohammedJSharekh@users.noreply.github.com>
2020-03-09 19:07:56 +0300

    Add files via upload

---
+++ build/precache-manifest.47c4a302bf10ed194e6a81db22b73a71.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02766ac83faa6ff306ba8cbbbbf5f3db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8cb53986ea16fe57aed",
+    "url": "/static/css/2.81e2c8a5.chunk.css"
+  },
+  {
+    "revision": "83ad872d7be6b10fa2ff",
+    "url": "/static/css/main.1c15f17d.chunk.css"
+  },
+  {
+    "revision": "a8cb53986ea16fe57aed",
+    "url": "/static/js/2.0a65e042.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.0a65e042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ad872d7be6b10fa2ff",
+    "url": "/static/js/main.dc1b8e37.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "d94aec6899696c35adcdd0d5ce49e312",
+    "url": "/static/media/Abract01.d94aec68.png"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/static/media/JS1.2d2e31cc.svg"
+  },
+  {
+    "revision": "eeb4dad66cf2ff3ee47b7ac7906913a0",
+    "url": "/static/media/PatrickHand-Regular.eeb4dad6.ttf"
+  },
+  {
+    "revision": "399f0263c775a957cc13d954bfa2d696",
+    "url": "/static/media/Screenshot (124)-min.399f0263.png"
+  },
+  {
+    "revision": "bd71ea66d4994832d7bf9d46fd4ce50c",
+    "url": "/static/media/Screenshot (136)-min.bd71ea66.png"
+  },
+  {
+    "revision": "2b1589ac540a633c353ff7f56e8a3c23",
+    "url": "/static/media/Screenshot (224).2b1589ac.png"
+  },
+  {
+    "revision": "89c1c2e7ce11c285c05e8d7ccb14eb75",
+    "url": "/static/media/Screenshot (232).89c1c2e7.png"
+  },
+  {
+    "revision": "963e7a65b410bc9f82a738de8c225114",
+    "url": "/static/media/Screenshot (24)-min.963e7a65.png"
+  },
+  {
+    "revision": "22c6e70478d85bb852ac6873e9c3d3fb",
+    "url": "/static/media/Screenshot (54)-min.22c6e704.png"
+  },
+  {
+    "revision": "1c66a11336d766b7e27f78b2b7960291",
+    "url": "/static/media/Screenshot (81)-min.1c66a113.png"
+  },
+  {
+    "revision": "592286b97127c75c7ce48d45ada423c7",
+    "url": "/static/media/backgroundimg.592286b9.png"
+  },
+  {
+    "revision": "50953027678f2e493ea34b63733f26cd",
+    "url": "/static/media/banner-image.50953027.png"
+  },
+  {
+    "revision": "4877d96a3ad43bdeeac5e90f629e52ba",
+    "url": "/static/media/clip-programming.4877d96a.png"
+  },
+  {
+    "revision": "3f8e68f49f7dba3390a43f8fcc1a14f7",
+    "url": "/static/media/css3.3f8e68f4.png"
+  },
+  {
+    "revision": "454a400ca987d4271e54045982ea3876",
+    "url": "/static/media/html5.454a400c.png"
+  },
+  {
+    "revision": "6cbb89e9782c49314fb621832fc36218",
+    "url": "/static/media/react.6cbb89e9.png"
+  }
+]);
